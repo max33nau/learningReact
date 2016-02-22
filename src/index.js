5c8941e48910ff4c8a8c9ae5ef1effad338c387f
max33nau <mtj29@nau.edu>
2016-02-22 12:26:24 -0800

    react router initial setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
+import App from "./app";
+
+render((
+    <Router history={hashHistory}>
+      <Route path='/' component {App} />
+    </Router>
+), document.getElementById('app'));
