77ca56244d6d31daed9df3bae4725eff7189acc9
max33nau <mtj29@nau.edu>
2016-02-22 13:34:02 -0800

    basic routes completed with react router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-import React from 'react';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
-
-import App from "./app";
+import React from 'react';
+import Home from './modules/Home'
+import App from './modules/App';
+import About from './modules/About';
+import Repos from './modules/Repos';
+import Repo from './modules/Repo';
 
 render((
     <Router history={hashHistory}>
-      <Route path='/' component {App} />
+      <Route path='/' component={App}>
+        <IndexRoute component={Home}/>
+        <Route path='/repos' component={Repos}>
+          <Route path='/repos/:userName/:repoName' component={Repo}/>
+        </Route>
+        <Route path='/about' component={About}/>
+      </Route>
     </Router>
 ), document.getElementById('app'));
